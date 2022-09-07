9f6e2709f067b49fd4a24a22ac18909c0ba7ed01
Gopinath Athiappan <109467048+Gopinath-athiappan@users.noreply.github.com>
2022-09-07 23:34:24 +0530

    Characters code added

---
+++ Characters.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+char ch ;
+char *s[100];
+char *sen[100];
+scanf("%c\n%s\n",&ch,&s);
+gets(sen);
+printf("%c\n%s\n",ch,s);
+puts(sen);
+return 0;
+}
